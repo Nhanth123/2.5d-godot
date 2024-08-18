5c3bfa150ec59a130570cce68fc655d8d3c8127d
nhan.trinh <hongnhan50@gmail.com>
2024-08-18 14:56:14 +0700

    Feature: Implement sword and inputmap for player.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,7 +2,6 @@ extends CharacterBody3D
 
 @onready var root_node: Node3D = $Visual/RootNode
 
-
 const  SPEED = 10
 const  JUMP_VELOCITY = 22
 
@@ -19,19 +18,17 @@ func _physics_process(delta):
 		else:
 			root_node.rotation = Vector3(0,PI,0)
 #endregion
-		
-	
+
 	if not is_on_floor():
 		velocity.y -= gravity * delta * 8
 	
-	if Input.is_action_just_pressed("ui_accept")and is_on_floor():
+	if Input.is_action_just_pressed("jump")and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up","ui_down")
-	var direction =  (transform.basis * Vector3(input_dir.x, 0 , input_dir.y)).normalized()
 	
-	if direction:
-		velocity.x = direction.x * SPEED
+	var horizontalInput = Input.get_axis("move_left", "move_right")
+		
+	if horizontalInput:
+		velocity.x = horizontalInput.x * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, 1)
 	
