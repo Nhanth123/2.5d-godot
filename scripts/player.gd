bcdbf3ee4977f215e2e0b0602049e925a68d061d
nhan.trinh <hongnhan50@gmail.com>
2024-08-18 17:47:50 +0700

    Feature: implement player's movement

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,15 @@
 extends CharacterBody3D
 
 @onready var root_node: Node3D = $Visual/RootNode
+@onready var animation_tree: AnimationTree = $Visual/AnimationTree
 
 const  SPEED = 10
 const  JUMP_VELOCITY = 22
 
-
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+func _process(_delta):
+	animation_tree.set("parameters/StateMachine/GroundMovement/blend_position", abs(velocity.x))
 
 func _physics_process(delta):
 	
@@ -28,7 +31,8 @@ func _physics_process(delta):
 	var horizontalInput = Input.get_axis("move_left", "move_right")
 		
 	if horizontalInput:
-		velocity.x = horizontalInput.x * SPEED
+		horizontalInput = int(horizontalInput)
+		velocity.x = horizontalInput * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, 1)
 	
