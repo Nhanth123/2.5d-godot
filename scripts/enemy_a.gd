a36e19e41c18e4b6cde9e05e3d0b277da8e21981
nhan.trinh <hongnhan50@gmail.com>
2024-08-30 14:50:31 +0700

    Feature: Implement enemy for game play.

--- scripts/enemy_a.gd
+++ scripts/enemy_a.gd
@@ -1,12 +1,45 @@
 extends CharacterBody3D
 
 
-const SPEED = 1.5
+const SPEED = 1.4
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-func _process(delta: float) -> void:
+@onready var ray_cast_3d_forward = $CollisionShape3D/RayCast3D_Forward
+@onready var ray_cast_3d_downward = $CollisionShape3D/RayCast3D_Downward
+@onready var root_node: Node3D = $Visual/RootNode
+@onready var collision_shape_3d: CollisionShape3D = $CollisionShape3D
+@onready var animation_player: AnimationPlayer = $Visual/AnimationPlayer
+
+
+var direction
+var facingRight = true
+
+func _ready() -> void:
+	animation_player.play("NPC_01_WALK")
+	
+func _process(delta: float):
 	if not is_on_floor():
 		velocity.y -= gravity * delta * 5
 	
+	
+	if ray_cast_3d_forward.is_colliding() || ray_cast_3d_downward.is_colliding() == false:
+		facingRight = !facingRight
+	
+		
+	if facingRight:
+		direction = 1
+		root_node.rotation = Vector3(0, 0, 0)
+		collision_shape_3d.rotation = Vector3(0 , 0, 0)
+	else:
+		direction = -1
+		root_node.rotation = Vector3(0, PI, 0)
+		collision_shape_3d.rotation = Vector3(0, PI, 0)
+
+	velocity.x = direction * SPEED
+	
 	move_and_slide()
+
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	body.applyDamage()
